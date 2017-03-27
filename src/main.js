d6aa19d20e704f5c641b0b3ac9c66069899da92a
Tatsuro Baba <harakirisoul@gmail.com>
2017-03-27 15:06:27 +0900

    Introduce Material-UI

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <div>Hello SPA!!</div>,
-  document.getElementById('app')
-)
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+
+injectTapEventPlugin();
+
+class App extends React.Component {
+  render() {
+    return(
+      <MuiThemeProvider>
+        <RaisedButton label="Yes" primary={true} fullWidth={true} />
+      </MuiThemeProvider>
+    );
+  }
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
